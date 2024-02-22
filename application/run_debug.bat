a1e6f78c1306547aa7b162d612efd1da534c94b0
Camilo <camilo.bedoya@rwth-aachen.de>
2024-02-22 15:21:27 +0100

    Fixed issue with index.html and run_debug.bat

--- application/run_debug.bat
+++ application/run_debug.bat
@@ -1,4 +1,11 @@
-set environment=development
-set FLASK_APP=main.py
-set FLASK_DEBUG=1
-flask run
+@echo off
+REM Set the path to the Python executable within the virtual environment
+set VENV_PYTHON=C:\Users\Camilo Bedoya\.conda\envs\mtesispy311\python.exe
+
+REM Set environment variables
+set "environment=development"
+set "FLASK_APP=main.py"
+set "FLASK_DEBUG=1"
+
+REM Run Flask using the Python executable from the virtual environment
+"%VENV_PYTHON%" -m flask run
